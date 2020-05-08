2cded4b15cff66586c3c8653fcc2f3f1a51381dd
Harrison <hmseaborn@gmail.com>
2020-05-08 08:12:30 -0700

    server up
    changed scripts to have a server for nodemon and a start for node... start is for heroku use.. Index file up listening on 5005 server file created in /api/server

---
+++ index.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const server = require("./api/server.js");
+const port = process.env.PORT || 5005;
+
+server.listen(port, () => {
+  console.log(`\n* Server Running on http://localhost:${port}\n`);
+});
