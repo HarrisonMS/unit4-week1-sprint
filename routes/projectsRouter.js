fbcaa18a39e019994ac18b59432d9d5d3677a207
Harrison <hmseaborn@gmail.com>
2020-05-08 09:11:34 -0700

    Added  get to ap/projects/
    gets all projects in database

--- routes/projectsRouter.js
+++ routes/projectsRouter.js
@@ -2,4 +2,17 @@ const express = require("express");
 const router = express.Router();
 const Projects = require("../data/helpers/projectModel");
 
+//get all Projects in database
+
+router.get("/", (req, res) => {
+  Projects.get()
+    .then((projects) => {
+      res.status(200).json(projects);
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.status(500).json({ errorMessage: error.message });
+    });
+});
+
 module.exports = router;
