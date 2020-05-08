52960ca63a2b93cfd2fb327258bd57468702df07
Harrison <hmseaborn@gmail.com>
2020-05-08 08:39:07 -0700

    skeleton of actionsRouter created`
    /routes/actionsRouter have yet to attacth to server file

--- data/helpers/validatePutMid.js
+++ data/helpers/validatePutMid.js
@@ -1,14 +1,16 @@
 function validatePutMid(req, res, next) {
   if (!req.body) {
+    res.status(400).json({
+      message: "there is no post data in the body as far as I can tell",
+    });
+  } else if (!req.body.name) {
     res
       .status(400)
-      .json({
-        message: "there is no post data in the body as far as I can tell",
-      });
-  } else if (!req.body.name) {
-    res.status(400).json({ message: "name required buddy" });
+      .json({ message: "name required buddy to update the project" });
   } else if (!req.body.description) {
-    res.status(400).json({ message: "description is needed bud" });
+    res
+      .status(400)
+      .json({ message: "description is needed bud to update the project" });
   } else {
     req.body.user_id = req.user.id;
     next();
