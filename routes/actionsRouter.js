599fa2e267d98b11c0c8ae32866ffc47d23df7c6
Harrison <hmseaborn@gmail.com>
2020-05-08 10:12:38 -0700

    get all actions endpoint created
    located in routes/actionsRouter.. enpoint reads as GET /api/actions

--- routes/actionsRouter.js
+++ routes/actionsRouter.js
@@ -2,4 +2,16 @@ const express = require("express");
 const router = express.Router();
 const Actions = require("../data/helpers/actionModel");
 
+//get all actions from the database
+router.get("/", (req, res) => {
+  Actions.get()
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.status(500).json({ errorMessage: error.message });
+    });
+});
+
 module.exports = router;
